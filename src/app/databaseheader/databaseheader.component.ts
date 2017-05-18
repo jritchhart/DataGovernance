ae4a0c4b6aa9e02aab3c926da92d2096cecca2a5
jritchhart <jritchhart@comcast.net>
2017-05-18 07:20:12 -0500

    Add files via upload

---
+++ src/app/databaseheader/databaseheader.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit , Input } from '@angular/core';
+
+@Component({
+  selector: 'database-header',
+  templateUrl: './app/databaseheader/databaseheader.component.html',
+  styleUrls: ['./app/databaseheader/databaseheader.component.css']
+
+})
+export class databaseheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
